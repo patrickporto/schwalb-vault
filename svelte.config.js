3b99863c19556e4d47bfe1cfd79265b83f7d9149
Patrick Porto <patrick.s.porto@gmail.com>
2025-12-29 03:33:29 -0300

    chore: Update SvelteKit adapter fallback from `index.html` to `404.html`.

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
   kit: {
     adapter: adapter({
-      fallback: 'index.html'
+      fallback: '404.html'
     }),
     paths: {
       base: process.env.NODE_ENV === "production" ? "/schwalb-vault" : "",
