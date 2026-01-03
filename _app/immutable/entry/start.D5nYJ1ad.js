0e4b2fae6e06ee2ac9d928ff942262778fb0bfbb
patrickporto <patrickporto@users.noreply.github.com>
2026-01-03 19:09:22 +0000

    deploy: 21272f57853f0bd1865a191073550e455971a194

--- _app/immutable/entry/start.D5nYJ1ad.js
+++
@@ -1,1 +0,0 @@
-import{l as o,c as r}from"../chunks/DlL1WpJV.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.pQDmIy1e.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/jIF0cVk1.js";export{o as load_css,r as start};
